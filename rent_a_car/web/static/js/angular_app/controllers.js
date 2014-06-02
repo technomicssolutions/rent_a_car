1d6d990875391c03d01c2f0b2bbe5f7f4ac70cca
geethu <geethu@technomicssolutions.com>
2014-06-02 17:44:15 +0530

    changes to the controller and template

--- rent_a_car/web/static/js/angular_app/controllers.js
+++ rent_a_car/web/static/js/angular_app/controllers.js
@@ -52,8 +52,59 @@ function AddClientController($scope, $http, $location) {
         })
 	}
 
-	$scope.add_client = function() {
+	$scope.validate_client_form = function() {
+		if ($scope.client.name == '' || $scope.client.name == undefined) {
+			$scope.validation_error = 'Please enter the name of the client';
+			return false;
+		} else if ($scope.client.nationality == '' || $scope.client.nationality == undefined) {
+			$scope.validation_error = 'Please enter the nationality';
+			return false;
+		} else if ($scope.client.dob == '' || $scope.client.dob == undefined) {
+			$scope.validation_error = 'Please enter the date of birth';
+			return false;
+		} else if ($scope.client.home_address == '' || $scope.client.home_address == undefined) {
+			$scope.validation_error = 'Please enter the home address';
+			return false;
+		} else if ($scope.client.home_ph_no == '' || $scope.client.home_ph_no == undefined) {
+			$scope.validation_error = 'Please enter the Tel. no(home)';
+			return false;
+		} else if ($scope.client.work_address == '' || $scope.client.work_address == undefined) {
+			$scope.validation_error = 'Please enter work address';
+			return false;
+		} else if ($scope.client.work_ph_no == '' || $scope.client.work_ph_no == undefined) {
+			$scope.validation_error = 'Please enter Tel. no(work)';
+			return false;
+		} else if ($scope.client.license_no == '' || $scope.client.license_no == undefined) {
+			$scope.validation_error = 'Please enter License  no.';
+			return false;
+		} else if ($scope.client.license_type == '' || $scope.client.license_type == undefined) {
+			$scope.validation_error = 'Please choose License Type';
+			return false;
+		} else if ($scope.client.date_of_license_issue == '' || $scope.client.date_of_license_issue == undefined) {
+			$scope.validation_error = 'Please choose Date of license issued';
+			return false;
+		} else if ($scope.client.issued_by == '' || $scope.client.issued_by == undefined) {
+			$scope.validation_error = 'Please enter issued by';
+			return false;
+		} else if ($scope.client.expiry_date == '' || $scope.client.expiry_date == undefined) {
+			$scope.validation_error = 'Please choose expiry date';
+			return false;
+		} else if ($scope.client.passport_no == '' || $scope.client.passport_no == undefined) {
+			$scope.validation_error = 'Please enter Passport no.';
+			return false;
+		} else if ($scope.client.passport_issued_date == '' || $scope.client.passport_issued_date == undefined) {
+			$scope.validation_error = 'Please choose Passport Issued Date';
+			return false;
+		} else if ($scope.client.place_of_issue == '' || $scope.client.place_of_issue == undefined) {
+			$scope.validation_error = 'Please enter the Place of passport issued';
+			return false;
+		}
+		return true;
+	}
 
+	$scope.add_client = function() {
+		$scope.is_valid = $scope.validate_client_form();  
+		console.log($scope.is_valid);
 	}
 
 }
