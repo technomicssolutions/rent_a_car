e9964b289373945796f7b446f0e7610727cc0552
geethu <geethu@technomicssolutions.com>
2014-06-06 17:15:47 +0530

    Added client popup in rent agreement

--- rent_a_car/web/static/js/angular_app/controllers.js
+++ rent_a_car/web/static/js/angular_app/controllers.js
@@ -118,6 +118,94 @@ add_vehicle = function($scope, $http, from) {
         });
 	}
 }
+validate_client_form = function($scope, $http) {
+	$scope.client.dob = $$('#dob')[0].get('value');
+	$scope.client.date_of_license_issue = $$('#date_of_license_issue')[0].get('value');
+	$scope.client.expiry_date = $$('#expiry_date')[0].get('value');
+	$scope.client.passport_issued_date = $$('#passport_issued_date')[0].get('value');
+	if ($scope.client.name == '' || $scope.client.name == undefined) {
+		$scope.validation_error = 'Please enter the name of the client';
+		return false;
+	} else if ($scope.client.nationality == '' || $scope.client.nationality == undefined) {
+		$scope.validation_error = 'Please enter the nationality';
+		return false;
+	} else if ($scope.client.dob == '' || $scope.client.dob == undefined) {
+		$scope.validation_error = 'Please enter the date of birth';
+		return false;
+	} else if ($scope.home_address == '' || $scope.home_address == undefined) {
+		$scope.validation_error = 'Please enter the home address';
+		return false;
+	} else if ($scope.client.home_ph_no == '' || $scope.client.home_ph_no == undefined) {
+		$scope.validation_error = 'Please enter the Tel. no(home)';
+		return false;
+	} else if ($scope.client.license_no == '' || $scope.client.license_no == undefined) {
+		$scope.validation_error = 'Please enter License  no.';
+		return false;
+	} else if ($scope.client.license_type == '' || $scope.client.license_type == undefined) {
+		$scope.validation_error = 'Please choose License Type';
+		return false;
+	} else if ($scope.client.date_of_license_issue == '' || $scope.client.date_of_license_issue == undefined) {
+		$scope.validation_error = 'Please choose Date of license issued';
+		return false;
+	} else if ($scope.client.issued_by == '' || $scope.client.issued_by == undefined) {
+		$scope.validation_error = 'Please enter issued by';
+		return false;
+	} else if ($scope.client.expiry_date == '' || $scope.client.expiry_date == undefined) {
+		$scope.validation_error = 'Please choose expiry date';
+		return false;
+	} else if ($scope.client.passport_no == '' || $scope.client.passport_no == undefined) {
+		$scope.validation_error = 'Please enter Passport no.';
+		return false;
+	} else if ($scope.client.passport_issued_date == '' || $scope.client.passport_issued_date == undefined) {
+		$scope.validation_error = 'Please choose Passport Issued Date';
+		return false;
+	} else if ($scope.client.place_of_issue == '' || $scope.client.place_of_issue == undefined) {
+		$scope.validation_error = 'Please enter the Place of passport issued';
+		return false;
+	}
+	return true;
+}
+
+add_client = function($scope, $http, from) {
+	$scope.is_valid = validate_client_form($scope, $http);  
+	if ($scope.is_valid) {
+		$scope.validation_error = '';
+		params = { 
+			'client_work_address': $scope.work_address,
+			'client_home_address': $scope.home_address,
+            'client_details': angular.toJson($scope.client),
+            "csrfmiddlewaretoken" : $scope.csrf_token,
+        }
+        $http({
+            method : 'post',
+            url : "/add_client/",
+            data : $.param(params),
+            headers : {
+                'Content-Type' : 'application/x-www-form-urlencoded'
+            }
+        }).success(function(data, status) {
+            
+            if (data.result == 'error'){
+                $scope.error_flag=true;
+                $scope.message = data.message;
+            } else {
+                $scope.error_flag=false;
+                $scope.message = '';
+                if(from != 'client_popup') {
+                	document.location.href ='/clients/';
+                } else {
+                	$scope.client_data = data.client_data[0];
+	            	get_clients($scope, $http);
+	            	$scope.client = data.client_data[0];
+	            	$scope.close_popup_add_client();
+                }
+                	
+            }
+        }).error(function(data, status){
+            $scope.validation_error = data.message;
+        });
+	}
+}
 
 function AddClientController($scope, $http, $location) {
 
@@ -619,6 +707,38 @@ function RentAgreementController($scope, $http, $location) {
             format:'%d/%m/%Y %H:%M',
             canAlwaysGoUp: ['months', 'years']
         });
+        new Picker.Date($$('#dob'), {
+            timePicker: false,
+            positionOffset: {x: 5, y: 0},
+            pickerClass: 'datepicker_bootstrap',
+            useFadeInOut: !Browser.ie,
+            format:'%d/%m/%Y',
+            canAlwaysGoUp: ['months', 'years']
+        });
+        new Picker.Date($$('#passport_issued_date'), {
+            timePicker: false,
+            positionOffset: {x: 5, y: 0},
+            pickerClass: 'datepicker_bootstrap',
+            useFadeInOut: !Browser.ie,
+            format:'%d/%m/%Y',
+            canAlwaysGoUp: ['months', 'years']
+        });
+      	new Picker.Date($$('#expiry_date'), {
+            timePicker: false,
+            positionOffset: {x: 5, y: 0},
+            pickerClass: 'datepicker_bootstrap',
+            useFadeInOut: !Browser.ie,
+            format:'%d/%m/%Y',
+            canAlwaysGoUp: ['months', 'years']
+        });
+        new Picker.Date($$('#date_of_license_issue'), {
+            timePicker: false,
+            positionOffset: {x: 5, y: 0},
+            pickerClass: 'datepicker_bootstrap',
+            useFadeInOut: !Browser.ie,
+            format:'%d/%m/%Y',
+            canAlwaysGoUp: ['months', 'years']
+        });
 
 	}
 	$scope.add_new_vehicle = function() {
@@ -670,6 +790,26 @@ function RentAgreementController($scope, $http, $location) {
 		$('#new_vehicle_type').hide();
 		$('#new_vehicle').show();
 	}
+	$scope.add_client = function() {
+		$scope.client_popup = new DialogueModelWindow({
+            'dialogue_popup_width': '36%',
+            'message_padding': '0px',
+            'left': '28%',
+            'top': '40px',
+            'height': 'auto',
+            'content_div': '#new_client'
+        });
+        var height = $(document).height();
+        $scope.client_popup.set_overlay_height(height);
+        $scope.client_popup.show_content();
+	}
+	$scope.save_client = function() {
+		add_client($scope, $http, 'client_popup');
+	}
+ 
+	$scope.close_popup_add_client = function() {
+		$scope.client_popup.hide_popup();
+	}
 	$scope.close_popup_add_vehicle =  function() {
 		$scope.popup.hide_popup();
 	}
@@ -692,7 +832,7 @@ function RentAgreementController($scope, $http, $location) {
 	$scope.with_driver_mode = function(mode) {
 		if (mode == 'yes') {
 			$scope.driver_details_needed = false;
-			new Picker.Date($$('#dob'), {
+			new Picker.Date($$('#driver_dob'), {
 	            timePicker: false,
 	            positionOffset: {x: 5, y: 0},
 	            pickerClass: 'datepicker_bootstrap',
